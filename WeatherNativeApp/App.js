3be46660687425280b36c19557b4e4f01a424ba5
SeunghoLee <jimmylee871124@gmail.com>
2019-05-11 23:36:42 +0900

    [FEAT]Weather app based on expo

---
+++ WeatherNativeApp/App.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import Weather from "./Weather";
+
+const API_KEY = "94a4afb8685d99c9ff279e1c33303429";
+
+export default class App extends React.Component {
+  state = {
+    isLoaded: false,
+    error: null,
+    temperature: null,
+    name: null
+  }
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this._getWeather(position.coords.latitude, position.coords.longitude)
+      },
+      error => {
+        this.setState({
+          error:error
+        })
+      }
+    );
+  }
+
+  _getWeather = (lat, lon) => {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`
+    )
+    .then(response => response.json())
+    .then(json =>{
+      this.setState({
+        temperature: json.main.temp,
+        name: json.weather[0].main,
+        isLoaded: true
+      });
+    });
+  }
+  render() {
+    const { isLoaded, error, temperature, name } = this.state;
+    return (
+      <View style={styles.container}>
+        <StatusBar hidden={true} />
+        {isLoaded ? (
+          <Weather weatherName={name} temp={temperature} />
+        ) : (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>Getting the fucking weather</Text>
+            {error ? <Text style={styles.errorText}>{error}</Text> : null}
+          </View>
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  errorText: {
+    color:'red',
+    backgroundColor:'transparent',
+    marginBottom: 40
+  },
+  loading:{
+    flex:1,
+    backgroundColor: '#FDF6AA',
+    justifyContent: 'flex-end',
+    paddingLeft:25
+  },
+  loadingText:{
+    fontSize:38,
+    marginBottom:100
+  }
+});
